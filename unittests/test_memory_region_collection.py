db71f5014638133b41e1181d1b76776d7a814a3e
Petrut Antoniu Bogdan <petrutantoniu8@gmail.com>
2014-08-07 16:41:28 +0100

    Added last test skeletons for data spec

---
+++ unittests/test_memory_region_collection.py
@@ -0,0 +1,11 @@
+import unittest
+from data_specification.memory_region_collection import MemoryRegionCollection
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()

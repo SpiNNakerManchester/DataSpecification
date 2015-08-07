fa5391c901c00c4aac2ef86f2d9b2aed49efa234
Paul92 <paul.gafton@gmail.com>
2015-08-07 15:30:19 +0100

    Added data spec sender Command class

---
+++ data_specification/data_spec_sender/command.py
@@ -0,0 +1,13 @@
+class Command:
+
+    def __init__(self, cmd):
+        self._value = cmd
+
+    def get_value(self):
+        return self._value
+
+    def get_length(self):
+        return (self._value >> 28) & 0x3
+
+    def get_opcode(self):
+        return (self._value >> 20) & 0xFF

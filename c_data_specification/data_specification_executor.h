d87f4a8bd5ebf8ed6a86ce6aea28f2a26a228aaa
Paul92 <paul.gafton@gmail.com>
2015-08-15 19:49:36 +0100

    Added conditional inclusion and macro definitions in data_specificatoin_executor.h to be able to compile in emulate mode

--- c_data_specification/data_specification_executor.h
+++ c_data_specification/data_specification_executor.h
@@ -12,10 +12,32 @@
 #include "constants.h"
 #include "commands.h"
 #include <stdint.h>
-#include <spinnaker.h>
-#include <data_specification.h>
+
+#ifdef EMULATE
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+typedef uint32_t* address_t;
+#define spin1_exit(code) exit(code)
+#define spin1_memcpy(destination, source, size) memcpy((destination), (source), (size))
+#define rt_error(error_code) exit((error_code))
+#define RTE_ABORT -1
+#define log_info(message, ...) printf("[INFO]    ", message, ##__VA_ARGS__)
+#define log_error(message, ...) printf("[ERROR]    ", message, ##__VA_ARGS__)
+#define log_debug(message, ...) printf("[DEBUG]    ", message, ##__VA_ARGS__)
+#define sark_xalloc(heap, size, tag, flag) malloc((size))
+#define sark_alloc(count, size) malloc((count) * (size))
+#define sark_xfree(heap, ptr, flag) free((ptr))
+#define sark_free(ptr) free((ptr))
+#endif
+
+
+#ifndef EMULATE
 #include <spin1_api.h>
 #include <debug.h>
+#include <spinnaker.h>
+#include <data_specification.h>
+#endif
 
 // Stores the details of a command.
 struct Command {
@@ -33,6 +55,7 @@ struct MemoryRegion {
     int free;
     uint8_t* write_pointer;
 };
+
 
 struct Constructor {
     address_t start_address;

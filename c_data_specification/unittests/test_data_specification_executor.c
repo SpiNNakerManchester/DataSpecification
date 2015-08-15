150c70cda5845be167d3091e71cc3ec0457b1b95
Paul92 <paul.gafton@gmail.com>
2015-08-15 22:47:45 +0100

    Added test_get_next_command()

--- c_data_specification/unittests/test_data_specification_executor.c
+++ c_data_specification/unittests/test_data_specification_executor.c
@@ -10,6 +10,9 @@ uint8_t command_get_src2Reg(uint32_t command);
 int command_dest_in_use(uint32_t command);
 int command_src1_in_use(uint32_t command);
 int command_src2_in_use(uint32_t command);
+struct Command get_next_command();
+
+extern address_t command_pointer;
 
 void test_command_get_length() {
     cut_assert_equal_int(0x00, command_get_length(0x01333567));
@@ -126,6 +129,57 @@ void test_command_src2_in_use() {
     cut_assert_equal_int(0x00, command_src2_in_use(0x0FA62345));
     cut_assert_equal_int(0x01, command_src2_in_use(0xF9972345));
     cut_assert_equal_int(0x01, command_src2_in_use(0xF99F2345));
+}
+
+void test_get_next_command() {
+    uint32_t commands[] = {0x9DB703A7, 0x2B52EA07,
+                           0xA6AA233C, 0x343A207B, 0x6BD67CE5,
+                           0x58FE1B19, 0x263CBFCD,
+                           0x920A1C38, 0x6BC65B04,
+                           0x5C84BE6A, 0x05E2FC3B,
+                           0xF7CD26BE, 0xC5C94996, 0x21ABFBBC, 0x000000F0};
+
+    command_pointer = commands;
+
+    struct Command cmd;
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x01, cmd.dataLength);
+    cut_assert_equal_int(0xDB, cmd.opCode);
+    cut_assert_equal_int(0x9DB703A7, cmd.cmdWord);
+    cut_assert_equal_int(0x2B52EA07, cmd.dataWords[0]);
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x02, cmd.dataLength);
+    cut_assert_equal_int(0x6A, cmd.opCode);
+    cut_assert_equal_int(0xA6AA233C, cmd.cmdWord);
+    cut_assert_equal_int(0x343A207B, cmd.dataWords[0]);
+    cut_assert_equal_int(0x6BD67CE5, cmd.dataWords[1]);
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x01, cmd.dataLength);
+    cut_assert_equal_int(0x8F, cmd.opCode);
+    cut_assert_equal_int(0x58FE1B19, cmd.cmdWord);
+    cut_assert_equal_int(0x263CBFCD, cmd.dataWords[0]);
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x01, cmd.dataLength);
+    cut_assert_equal_int(0x20, cmd.opCode);
+    cut_assert_equal_int(0x920A1C38, cmd.cmdWord);
+    cut_assert_equal_int(0x6BC65B04, cmd.dataWords[0]);
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x01, cmd.dataLength);
+    cut_assert_equal_int(0xC8, cmd.opCode);
+    cut_assert_equal_int(0x5C84BE6A, cmd.cmdWord);
+
+    cmd = get_next_command();
+    cut_assert_equal_int(0x03, cmd.dataLength);
+    cut_assert_equal_int(0x7C, cmd.opCode);
+    cut_assert_equal_int(0xF7CD26BE, cmd.cmdWord);
+    cut_assert_equal_int(0xC5C94996, cmd.dataWords[0]);
+    cut_assert_equal_int(0x21ABFBBC, cmd.dataWords[1]);
+    cut_assert_equal_int(0x000000F0, cmd.dataWords[2]);
 }
 
 

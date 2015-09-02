a110efaa422770e365b4c06008ad7d29a1a49ea9
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-09-02 07:53:50 +0100

    Setup for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="SpiNNaker_DataSpecification",
-    version="2015.004-rc-01",
+    version="2015.004",
     description="Specification of Memory Images",
     url="https://github.com/SpiNNakerManchester/DataSpecification",
     license="GNU GPLv3.0",

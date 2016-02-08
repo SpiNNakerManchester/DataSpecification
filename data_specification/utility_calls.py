71ce6f9e8bc94ce5cf48efaa3e89b9193ec70c10
alan-stokes <alan.barry.stokes@gmail.com>
2016-02-08 12:40:59 +0000

    added functionality to extract provenance data to the end user/to xml files.

--- data_specification/utility_calls.py
+++ data_specification/utility_calls.py
@@ -7,10 +7,11 @@ from data_specification import constants
 
 def get_region_base_address_offset(app_data_base_address, region):
     """
-    helper method which finds the address of the of a gvien region for the dsg
-    :param app_data_base_address: base address for
-    :param region:
+    helper method which finds the address of the of a given region for the dsg
+    :param app_data_base_address: base address for the core
+    :param region: the region id we're looking for
     :return:
     """
     return (app_data_base_address +
             constants.APP_PTR_TABLE_HEADER_BYTE_SIZE + (region * 4))
+

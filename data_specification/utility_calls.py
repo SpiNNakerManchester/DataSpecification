81501265f6dd31f6c2fa80b65d9f45910d558525
Christian <brenninc@cs.man.ac.uk>
2017-02-10 09:59:37 +0000

    Fixed docs for sphinx
    Missing space before params cause sphinx to ignore the params
    There must be a space after :param name:
    return must be :return: ...  or :return type:...
    closing quote should be on its own line

--- data_specification/utility_calls.py
+++ data_specification/utility_calls.py
@@ -8,10 +8,9 @@ from data_specification import constants
 def get_region_base_address_offset(app_data_base_address, region):
     """
     helper method which finds the address of the of a given region for the dsg
+
     :param app_data_base_address: base address for the core
     :param region: the region id we're looking for
-    :return:
     """
     return (app_data_base_address +
             constants.APP_PTR_TABLE_HEADER_BYTE_SIZE + (region * 4))
-

82f703b046d2b39271296b4028d5b76e4933612a
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-02-15 10:30:26 +0000

    API changed

--- unittests/test_file_data_reader.py
+++ unittests/test_file_data_reader.py
@@ -17,27 +17,32 @@ class TestFileDataReader(unittest.TestCase):
         if self.reader is not None:
             self.reader.close()
 
+    # ------------- TESTS START BELOW HERE -------------
+
     def test_read_one_byte(self):
         self.reader = FileDataReader(self._file('txt_one_byte'))
         stream = self.reader.read(1)
-        self.assertEqual(stream[0], ord('1'))
+
+        self.assertEqual(stream[0], '1')
 
     def test_readinto_one_byte(self):
         self.reader = FileDataReader(self._file('txt_one_byte'))
         ba = bytearray(1)
         self.reader.readinto(ba)
+
         self.assertEqual(len(ba), 1)
         self.assertEqual(ba[0], ord('1'))
 
     def test_read_five_bytes(self):
         self.reader = FileDataReader(self._file('txt_5_bytes'))
         stream = self.reader.read(5)
+
         self.assertEqual(len(stream), 5)
-        self.assertEqual(stream[0], ord('1'))
-        self.assertEqual(stream[1], ord('2'))
-        self.assertEqual(stream[2], ord('3'))
-        self.assertEqual(stream[3], ord('4'))
-        self.assertEqual(stream[4], ord('5'))
+        self.assertEqual(stream[0], '1')
+        self.assertEqual(stream[1], '2')
+        self.assertEqual(stream[2], '3')
+        self.assertEqual(stream[3], '4')
+        self.assertEqual(stream[4], '5')
 
     def test_read_from_empty_file(self):
         self.reader = FileDataReader(self._file('txt_empty'))
@@ -49,8 +54,8 @@ class TestFileDataReader(unittest.TestCase):
             self._file('txt_one_byte_from_multiple_bytes'))
         stream = self.reader.read(2)
         self.assertEqual(len(stream), 2)
-        self.assertEqual(stream[0], 240)
-        self.assertEqual(stream[1], 164)
+        self.assertEqual(stream[0], '\xf0')
+        self.assertEqual(stream[1], '\xa4')
 
 
 if __name__ == '__main__':

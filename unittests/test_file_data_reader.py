041fa794eae5bfac508c3e4b4507f48a9fb776c2
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-02-15 10:24:46 +0000

    Relativise the file handling correctly

--- unittests/test_file_data_reader.py
+++ unittests/test_file_data_reader.py
@@ -1,4 +1,6 @@
 import unittest
+import inspect
+import os.path
 
 from spinn_storage_handlers.file_data_reader import FileDataReader
 
@@ -6,25 +8,29 @@ from spinn_storage_handlers.file_data_reader import FileDataReader
 class TestFileDataReader(unittest.TestCase):
     def setUp(self):
         self.reader = None
+        self._dir = os.path.dirname(inspect.getfile(self.__class__))
+
+    def _file(self, filename):
+        return os.path.join(self._dir, "files_data_reader", filename)
 
     def tearDown(self):
         if self.reader is not None:
             self.reader.close()
 
     def test_read_one_byte(self):
-        self.reader = FileDataReader('files_data_reader/txt_one_byte')
+        self.reader = FileDataReader(self._file('txt_one_byte'))
         stream = self.reader.read(1)
         self.assertEqual(stream[0], ord('1'))
 
     def test_readinto_one_byte(self):
-        self.reader = FileDataReader('files_data_reader/txt_one_byte')
+        self.reader = FileDataReader(self._file('txt_one_byte'))
         ba = bytearray(1)
         self.reader.readinto(ba)
         self.assertEqual(len(ba), 1)
         self.assertEqual(ba[0], ord('1'))
 
     def test_read_five_bytes(self):
-        self.reader = FileDataReader('files_data_reader/txt_5_bytes')
+        self.reader = FileDataReader(self._file('txt_5_bytes'))
         stream = self.reader.read(5)
         self.assertEqual(len(stream), 5)
         self.assertEqual(stream[0], ord('1'))
@@ -34,13 +40,13 @@ class TestFileDataReader(unittest.TestCase):
         self.assertEqual(stream[4], ord('5'))
 
     def test_read_from_empty_file(self):
-        self.reader = FileDataReader('files_data_reader/txt_empty')
+        self.reader = FileDataReader(self._file('txt_empty'))
         stream = self.reader.read(1)
         self.assertEqual(len(stream), 0)
 
     def test_read_truncate(self):
         self.reader = FileDataReader(
-            'files_data_reader/txt_one_byte_from_multiple_bytes')
+            self._file('txt_one_byte_from_multiple_bytes'))
         stream = self.reader.read(2)
         self.assertEqual(len(stream), 2)
         self.assertEqual(stream[0], 240)

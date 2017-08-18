f302faf31499379fcbc98f0a2eead766ac12bff5
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-08-18 13:25:38 +0100

    Tracebacks should be done through the logger as much as possible

--- data_specification/data_specification_executor.py
+++ data_specification/data_specification_executor.py
@@ -1,11 +1,15 @@
-import struct
-import traceback
+import logging
 import numpy
+import struct
 
-from data_specification.data_specification_executor_functions \
+from .data_specification_executor_functions \
     import DataSpecificationExecutorFunctions as Dsef
-from data_specification import exceptions, constants
-from data_specification.enums import Commands
+from .constants import APPDATA_MAGIC_NUM, DSE_VERSION, END_SPEC_EXECUTOR, \
+    MAX_MEM_REGIONS, APP_PTR_TABLE_BYTE_SIZE, APP_PTR_TABLE_HEADER_BYTE_SIZE
+from .enums import Commands
+from .exceptions import DataSpecificationException
+
+logger = logging.getLogger(__name__)
 
 
 class DataSpecificationExecutor(object):
@@ -56,6 +60,7 @@ class DataSpecificationExecutor(object):
                     If the table pointer generated as data header exceeds the \
                     size of the available memory
         """
+        index = 0
         instruction_spec = self.spec_reader.read(4)
         while len(instruction_spec) != 0:
             # process the received command
@@ -66,20 +71,17 @@ class DataSpecificationExecutor(object):
             try:
                 # noinspection PyArgumentList
                 return_value = Commands(opcode).exec_function(self.dsef, cmd)
-            except ValueError:
-                traceback.print_exc()
-                raise exceptions.DataSpecificationException(
-                    "Invalid command 0x{0:X} while reading file {1:s}".format(
-                        cmd, self.spec_reader.filename))
-            except TypeError:
-                traceback.print_exc()
-                raise exceptions.DataSpecificationException(
+            except (ValueError, TypeError):
+                logger.debug("problem decoding opcode %d at index %d",
+                             cmd, index, exc_info=True)
+                raise DataSpecificationException(
                     "Invalid command 0x{0:X} while reading file {1:s}".format(
                         cmd, self.spec_reader.filename))
 
-            if return_value == constants.END_SPEC_EXECUTOR:
+            if return_value == END_SPEC_EXECUTOR:
                 break
             instruction_spec = self.spec_reader.read(4)
+            index += 4
 
     def get_region(self, region_id):
         """ Get a region with a given id
@@ -93,20 +95,18 @@ class DataSpecificationExecutor(object):
     def get_header(self):
         """ Get the header of the data as a numpy array
         """
-        return numpy.array(
-            [constants.APPDATA_MAGIC_NUM, constants.DSE_VERSION], dtype="<u4")
+        return numpy.array([APPDATA_MAGIC_NUM, DSE_VERSION], dtype="<u4")
 
     def get_pointer_table(self, start_address):
         """ Get the pointer table as a numpy array
 
         :param start_address: The base address of the data to be written
         """
-        pointer_table = numpy.zeros(constants.MAX_MEM_REGIONS, dtype="<u4")
-        pointer_table_size = constants.MAX_MEM_REGIONS * 4
-        next_free_offset = \
-            pointer_table_size + constants.APP_PTR_TABLE_HEADER_BYTE_SIZE
+        pointer_table = numpy.zeros(MAX_MEM_REGIONS, dtype="<u4")
+        pointer_table_size = MAX_MEM_REGIONS * 4
+        next_free_offset = pointer_table_size + APP_PTR_TABLE_HEADER_BYTE_SIZE
 
-        for i in xrange(constants.MAX_MEM_REGIONS):
+        for i in xrange(MAX_MEM_REGIONS):
             memory_region = self.dsef.mem_regions[i]
             if memory_region is not None:
                 pointer_table[i] = next_free_offset + start_address
@@ -122,8 +122,8 @@ class DataSpecificationExecutor(object):
         :return: size of the data that will be written to memory
         :rtype: unsigned int
         """
-        size = constants.APP_PTR_TABLE_BYTE_SIZE
-        for i in xrange(constants.MAX_MEM_REGIONS):
+        size = APP_PTR_TABLE_BYTE_SIZE
+        for i in xrange(MAX_MEM_REGIONS):
             memory_region = self.dsef.mem_regions[i]
             if memory_region is not None:
                 size += memory_region.allocated_size

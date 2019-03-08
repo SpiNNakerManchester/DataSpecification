0ca4358c1893ae66b0ee90e4f9507e4939b9e426
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2019-03-08 15:49:03 +0000

    Revert of PR 53 as it has upstream dependencies after all.

--- data_specification/utility_calls.py
+++ data_specification/utility_calls.py
@@ -26,7 +26,6 @@ def _mkdir(directory):
 
 def get_region_base_address_offset(app_data_base_address, region):
     """ Find the address of the of a given region for the DSG
-
     :param app_data_base_address: base address for the core
     :param region: the region ID we're looking for
     """
@@ -44,7 +43,6 @@ def get_data_spec_and_file_writer_filename(
         hostname, report_directory="TEMP", write_text_specs=False,
         application_run_time_report_folder="TEMP"):
     """ Encapsulates the creation of the DSG writer and the file paths
-
     :param processor_chip_x: x-coordinate of the chip
     :type processor_chip_x: int
     :param processor_chip_y: y-coordinate of the chip
@@ -76,41 +74,6 @@ def get_data_spec_and_file_writer_filename(
 
     # check if text reports are needed and if so initialise the report
     # writer to send down to DSG
-    report_writer = get_report_writer(
-        processor_chip_x, processor_chip_y, processor_id,
-        hostname, report_directory="TEMP", write_text_specs=False)
-
-    # build the file writer for the spec
-    spec = DataSpecificationGenerator(data_writer, report_writer)
-
-    return data_writer.filename, spec
-
-
-def get_report_writer(
-        processor_chip_x, processor_chip_y, processor_id,
-        hostname, report_directory="TEMP", write_text_specs=False):
-    """ check if text reports are needed and if so initialise the report
-        writer to send down to DSG
-
-    :param processor_chip_x: x-coordinate of the chip
-    :type processor_chip_x: int
-    :param processor_chip_y: y-coordinate of the chip
-    :type processor_chip_y: int
-    :param processor_id: The processor ID
-    :type processor_id: int
-    :param hostname: The hostname of the SpiNNaker machine
-    :type hostname: str
-    :param report_directory: the directory for the reports folder
-    :type report_directory: file path
-    :param write_text_specs:\
-        True if a textual version of the specification should be written
-    :type write_text_specs: bool
-    :return: the report_writer_object or None if not reporting
-    """
-    # pylint: disable=too-many-arguments
-
-    # check if text reports are needed and if so initialise the report
-    # writer to send down to DSG
     report_writer = None
     if write_text_specs:
         if report_directory == "TEMP":
@@ -121,4 +84,7 @@ def get_report_writer(
             os.path.join(new_report_directory, _RPT_TMPL.format(
                 hostname, processor_chip_x, processor_chip_y, processor_id)))
 
-    return report_writer
+    # build the file writer for the spec
+    spec = DataSpecificationGenerator(data_writer, report_writer)
+
+    return data_writer.filename, spec

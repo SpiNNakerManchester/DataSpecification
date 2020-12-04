ed707a76809e41eedbb68588b9cacabc990f0f28
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2020-12-04 10:07:11 +0000

    1.16 is last version to support 2.7
    https://numpy.org/devdocs/release/1.17.0-notes.html

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     install_requires=[
         'SpiNNUtilities >= 1!5.1.1, < 1!6.0.0',
         'SpiNNMachine >= 1!5.1.1, < 1!6.0.0',
-        "numpy == 1.18; python_version == '2.7'",
+        "numpy == 1.16; python_version == '2.7'",
         "numpy == 1.18; python_version == '3.5'",
         "numpy == 1.19; python_version == '3.6'",
         "numpy >= 1.19, <= 1.20; python_version == '3.7'",

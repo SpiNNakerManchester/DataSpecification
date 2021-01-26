56c3ce64417282d39d187cfdb0d59aa7af22c34e
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2021-01-26 12:03:31 +0000

    More compatibility gunk removal

--- setup.py
+++ setup.py
@@ -13,10 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 from collections import defaultdict
 import os
 

5cc7ae0be59c64271035ea0c2ed7c8df5ba43072
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2021-06-09 16:40:33 +0100

    call unittest_setup in every test for simplisity

--- unittests/test_cfg_checker.py
+++ unittests/test_cfg_checker.py
@@ -16,14 +16,13 @@
 import os
 import unittest
 from spinn_utilities.config_holder import run_config_checks
-from data_specification.config_setup import reset_configs
+from data_specification.config_setup import unittest_setup
 
 
 class TestCfgChecker(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        reset_configs()
+    def setUp(self):
+        unittest_setup()
 
     def test_cfg_check(self):
         unittests = os.path.dirname(__file__)
